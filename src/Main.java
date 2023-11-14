383643b5f8c1c8b62fc2a04045f52354e272060b
Бабкин Алексей <babkin.92@mail.ru>
2023-11-15 01:25:01 +0300

    new method added

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+//public class Main {
+//   public static void main(String[] args) {
+//     int ticketPrice = 13_676; //Цена билета
+//    int rubForBonusMiles = 20; //Количчество рублей необходимое для одной бонусной мили
+//int bonusMiles = ticketPrice / rubForBonusMiles; //Количество бонусных миль
+//System.out.println("Количество бонусных миль: " + bonusMiles);
+
+
+//}
+//}
+
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 13_676; //Цена билета
-        int rubForBonusMiles = 20; //Количчество рублей необходимое для одной бонусной мили
-        int bonusMiles = ticketPrice / rubForBonusMiles; //Количество бонусных миль
-        System.out.println("Количество бонусных миль: " + bonusMiles);
-
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
